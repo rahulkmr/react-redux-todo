6d8709530288dd671f9c49e722d34d8412741191
Rahul Kumar <rahul@thoughtnirvana.com>
2016-03-13 23:45:14 +0530

    Initialize store.

--- app/scripts/store/index.js
+++ app/scripts/store/index.js
@@ -1,7 +1,9 @@
 //@flow
 import {createStore} from 'redux'
 import todoReducers from '../reducers'
+import {startFetch} from '../actions'
 
 const store = createStore(todoReducers)
+startFetch(store)
 
 export default store
