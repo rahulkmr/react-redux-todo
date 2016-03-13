179b12b93bc67e342eedbd971aa34a66c23b0927
Rahul Kumar <rahul@thoughtnirvana.com>
2016-03-13 16:54:35 +0530

    Bug fix.

--- app/scripts/containers/FilterLink.js
+++ app/scripts/containers/FilterLink.js
@@ -5,14 +5,14 @@ import Link from '../components/Link'
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    active: ownProps.filter === state.visibilityFilter;
+    active: ownProps.filter === state.visibilityFilter
   };
 };
 
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    onClick: () => dispatch(setVisibilityFilter(ownProps.filter));
+    onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
   };
 };
 
