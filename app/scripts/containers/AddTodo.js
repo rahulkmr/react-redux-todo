e23e1926afc9c8384e8c410af600cfe046d06d08
Rahul Kumar <rahul@thoughtnirvana.com>
2016-03-14 00:22:06 +0530

    Post data to server.

--- app/scripts/containers/AddTodo.js
+++ app/scripts/containers/AddTodo.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
+import { todoApi } from '../constants'
+import $ from 'jquery'
+
+const postTodo = (dispatch, text) => {
+  $.ajax({
+    url: todoApi,
+    dataType: 'json',
+    data: {text: text},
+    type: 'POST'})
+    .then((data) => dispatch(addTodo(data.id, data.text, data.completed)))
+}
+
 
 let AddTodo = ({ dispatch }) => {
   let input
@@ -12,7 +24,7 @@ let AddTodo = ({ dispatch }) => {
         if (!input.value.trim()) {
           return
         }
-        dispatch(addTodo(input.value))
+        postTodo(dispatch, input.value)
         input.value = ''
       }}>
         <input ref={node => {
