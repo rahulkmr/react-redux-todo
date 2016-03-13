0a7eceb326a2c3ddcaad692e621332d8812b4197
Rahul Kumar <rahul@thoughtnirvana.com>
2016-03-13 16:55:35 +0530

    Mount todo app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,16 @@
 //@flow
-import React from 'react';
-import {render} from 'react-dom';
-import {CommentList} from './components/Comment';
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import todoReducers from './reducers'
+import TodoApp from './components/TodoApp'
 
-let comments = [
-  {id: 1, author: 'Diana Prince', text: 'How is everyone today!'},
-  {id: 2, author: 'Clark Kent', text: 'All good here.'},
-  {id: 3, author: 'Bruce Wayne', text: 'Hey everyone.'}
-];
+let store = createStore(todoReducers)
 
 render(
-  <CommentList comments={comments} />,
-    document.getElementById('root')
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
 );
-
