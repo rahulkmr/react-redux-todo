dba2e82347c0da0f05cb55b3e1894bb42285cecf
Rahul Kumar <rahul@thoughtnirvana.com>
2016-03-14 14:15:34 +0530

    Fetch todo when component mounts

--- app/scripts/components/TodoApp.js
+++ app/scripts/components/TodoApp.js
@@ -1,15 +1,24 @@
 //@flow
-import React from 'react'
+import React, { Component } from 'react'
 import Footer from './Footer'
 import VisibleTodoList from '../containers/VisibleTodoList'
 import AddTodo from '../containers/AddTodo'
+import { fetchTodos } from '../actions'
 
-const TodoApp = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
-)
+class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+      </div>
+    )
+  }
+
+  componentWillMount() {
+    fetchTodos()
+  }
+}
 
 export default TodoApp
