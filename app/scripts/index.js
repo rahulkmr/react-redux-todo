d395c564b0178af8c095655054a8d0112e3283c2
Rahul Kumar <rahul@thoughtnirvana.com>
2016-03-14 14:24:17 +0530

    Load todos

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,8 +4,9 @@ import {render} from 'react-dom'
 import {Provider} from 'react-redux'
 import TodoApp from './components/TodoApp'
 import store from './store'
+import {fetchTodos} from './actions'
 
-
+store.dispatch(fetchTodos())
 render(
   <Provider store={store}>
     <TodoApp />
