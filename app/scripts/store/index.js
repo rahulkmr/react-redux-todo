205b171d1576ce8e57d6b48c4a49f0d975c68ae7
Rahul Kumar <rahul@thoughtnirvana.com>
2016-03-14 14:37:51 +0530

    Logger middleware

--- app/scripts/store/index.js
+++ app/scripts/store/index.js
@@ -3,8 +3,11 @@ import {createStore, applyMiddleware} from 'redux'
 import todoReducers from '../reducers'
 import {fetchTodos} from '../actions'
 import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+
+const logger = createLogger()
 
 const store = createStore(todoReducers,
-                          applyMiddleware(thunkMiddleware))
+                          applyMiddleware(thunkMiddleware, logger))
 
 export default store
