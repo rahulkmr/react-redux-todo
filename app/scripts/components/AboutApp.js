0d1ae73136c5fbd097be04135a1ad4ca39fbfc1e
Rahul Kumar <rahul@thoughtnirvana.com>
2016-03-16 01:55:00 +0530

    Added App and About components.

---
+++ app/scripts/components/AboutApp.js
@@ -0,0 +1,17 @@
+//@flow
+import React, { Component } from 'react'
+
+class AboutApp extends Component {
+  render() {
+    return (
+      <div className="mdl-grid">
+        <div className="mdl-cell mdl-cell--4-offset">
+          <h2>Built with React and Redux</h2>
+          <p>Sample application to demonstrate React and Redux.</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AboutApp
---
+++ app/scripts/components/App.js
@@ -0,0 +1,34 @@
+//@flow
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+class App extends Component {
+  render() {
+    return (
+        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+            <header className="mdl-layout__header">
+            <div className="mdl-layout__header-row">
+                <span className="mdl-layout-title">TODO</span>
+                <div className="mdl-layout-spacer"></div>
+                <!-- Navigation. We hide it in small screens. -->
+                <nav className="mdl-navigation mdl-layout--large-screen-only">
+                <Link className="mdl-navigation__link" to="/about">About</Link>
+                </nav>
+            </div>
+            </header>
+            <div className="mdl-layout__drawer">
+                <span className="mdl-layout-title">TODO</span>
+                <nav className="mdl-navigation">
+                <a className="mdl-navigation__link" href="">About</a>
+                <a className="mdl-navigation__link" href="">Github</a>
+                </nav>
+            </div>
+            <main className="mdl-layout__content">
+            <div className="page-content">
+              {this.props.children}
+            </div>
+            </main>
+        </div>
+    )
+  }
+}
