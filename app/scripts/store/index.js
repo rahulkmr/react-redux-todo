6835639987104c6b6a649f714e7ad80b4d3c3e6f
Rahul Kumar <rahul@thoughtnirvana.com>
2016-03-16 01:54:41 +0530

    No redux-router for now.

--- app/scripts/store/index.js
+++ app/scripts/store/index.js
@@ -7,8 +7,10 @@ import { browserHistory } from 'react-router'
 import {routerReducer, syncHistoryWithStore} from 'react-router-redux'
 
 const logger = createLogger()
-const store = createStore(combineReducers({...reducers, routing: routerReducer}),
+//const store = createStore(combineReducers({...reducers, routing: routerReducer}),
+                          //applyMiddleware(thunkMiddleware, logger))
+//export const history = syncHistoryWithStore(browserHistory, store)
+const store = createStore(reducers,
                           applyMiddleware(thunkMiddleware, logger))
-export const history = syncHistoryWithStore(browserHistory, store)
 
 export default store
