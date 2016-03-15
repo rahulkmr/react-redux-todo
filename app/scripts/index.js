bcd7f0a1c94e5b23defdc9ffe9b743a9749b59ce
Rahul Kumar <rahul@thoughtnirvana.com>
2016-03-16 01:55:34 +0530

    Routes for app and about component.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,14 +2,20 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
+import App from './components/App'
 import TodoApp from './components/TodoApp'
-import store, {history} from './store'
-import { Router, Route, Link } from 'react-router'
+import AboutApp from './components/AboutApp'
+//import store, {history} from './store'
+import store from './store'
+import { Router, Route, IndexRoute, browserHistory, hashHistory } from 'react-router'
 
 render(
   <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={TodoApp} />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={TodoApp} />
+        <Route path="about" component={AboutApp} />
+      </Route>
     </Router>
   </Provider>,
   document.getElementById('root')
