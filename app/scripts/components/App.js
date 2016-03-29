8fe025cc7e484787fa5403ab0be7a983d9343b29
Rahul Kumar <rahul@thoughtnirvana.com>
2016-03-29 15:45:21 +0530

    Nav

--- app/scripts/components/App.js
+++ app/scripts/components/App.js
@@ -2,6 +2,25 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
+class Nav extends Component {
+  render() {
+    return (
+      <nav className={this.props.classes}>
+        <Link className="mdl-navigation__link" to="/" onlyActiveOnIndex={true} activeClassName="current-link">
+          Home
+        </Link>
+        <Link className="mdl-navigation__link" to="/about" activeClassName="current-link">
+          About
+        </Link>
+        <a className="mdl-navigation__link" href="https://github.com/rahulkmr/react-redux-todo">
+          GitHub
+        </a>
+      </nav>
+    )
+  }
+}
+
+
 class App extends Component {
   render() {
     return (
@@ -9,19 +28,14 @@ class App extends Component {
         <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
             <header className="mdl-layout__header">
             <div className="mdl-layout__header-row">
-                <Link to="/"><span className="mdl-layout-title">TODO</span></Link>
+                <Link id="title" to="/"><span className="mdl-layout-title">TODO</span></Link>
                 <div className="mdl-layout-spacer"></div>
-                <nav className="mdl-navigation mdl-layout--large-screen-only">
-                <Link className="mdl-navigation__link" to="/about">About</Link>
-                </nav>
+                <Nav classes="mdl-navigation mdl-layout--large-screen-only" />
             </div>
             </header>
             <div className="mdl-layout__drawer">
                 <span className="mdl-layout-title">TODO</span>
-                <nav className="mdl-navigation">
-                <a className="mdl-navigation__link" href="">About</a>
-                <a className="mdl-navigation__link" href="">Github</a>
-                </nav>
+                <Nav classes="mdl-navigation" />
             </div>
             <main className="mdl-layout__content">
             <div className="page-content">
